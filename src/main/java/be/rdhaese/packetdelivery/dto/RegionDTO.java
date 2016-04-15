6748409a165a8cf64fbdcfacccc87761c626376e
Robin D'Haese <robin.d'haese@realdolmen.com>
2016-04-15 16:50:26 +0200

    added multil language names to regionDTO

--- src/main/java/be/rdhaese/packetdelivery/dto/RegionDTO.java
+++ src/main/java/be/rdhaese/packetdelivery/dto/RegionDTO.java
@@ -9,20 +9,27 @@ import java.io.Serializable;import java.lang.Object;import java.lang.Override;im
  */
 public class RegionDTO implements Serializable{
 
-    private String name;
+    private String nameNl;
+    private String nameFr;
+    private String nameDe;
+    private String nameEn;
+
     private String code;
 
     public RegionDTO() {
     }
 
-    public RegionDTO(String name, String code) {
-        this.name = name;
+    public RegionDTO(String nameNl, String nameFr, String nameDe, String nameEn, String code) {
+        this.nameNl = nameNl;
+        this.nameFr = nameFr;
+        this.nameDe = nameDe;
+        this.nameEn = nameEn;
         this.code = code;
     }
 
     @Override
     public String toString() {
-        return name;
+        return code;
     }
 
     @Override
@@ -32,24 +39,58 @@ public class RegionDTO implements Serializable{
 
         RegionDTO regionDTO = (RegionDTO) o;
 
-        if (getName() != null ? !getName().equals(regionDTO.getName()) : regionDTO.getName() != null) return false;
+        if (getNameNl() != null ? !getNameNl().equals(regionDTO.getNameNl()) : regionDTO.getNameNl() != null)
+            return false;
+        if (getNameFr() != null ? !getNameFr().equals(regionDTO.getNameFr()) : regionDTO.getNameFr() != null)
+            return false;
+        if (getNameDe() != null ? !getNameDe().equals(regionDTO.getNameDe()) : regionDTO.getNameDe() != null)
+            return false;
+        if (getNameEn() != null ? !getNameEn().equals(regionDTO.getNameEn()) : regionDTO.getNameEn() != null)
+            return false;
         return !(getCode() != null ? !getCode().equals(regionDTO.getCode()) : regionDTO.getCode() != null);
 
     }
 
     @Override
     public int hashCode() {
-        int result = getName() != null ? getName().hashCode() : 0;
+        int result = getNameNl() != null ? getNameNl().hashCode() : 0;
+        result = 31 * result + (getNameFr() != null ? getNameFr().hashCode() : 0);
+        result = 31 * result + (getNameDe() != null ? getNameDe().hashCode() : 0);
+        result = 31 * result + (getNameEn() != null ? getNameEn().hashCode() : 0);
         result = 31 * result + (getCode() != null ? getCode().hashCode() : 0);
         return result;
     }
 
-    public String getName() {
-        return name;
+    public String getNameNl() {
+        return nameNl;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setNameNl(String nameNl) {
+        this.nameNl = nameNl;
+    }
+
+    public String getNameFr() {
+        return nameFr;
+    }
+
+    public void setNameFr(String nameFr) {
+        this.nameFr = nameFr;
+    }
+
+    public String getNameDe() {
+        return nameDe;
+    }
+
+    public void setNameDe(String nameDe) {
+        this.nameDe = nameDe;
+    }
+
+    public String getNameEn() {
+        return nameEn;
+    }
+
+    public void setNameEn(String nameEn) {
+        this.nameEn = nameEn;
     }
 
     public String getCode() {
